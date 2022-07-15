e89e88e2d2cbcdce210b3834660172e8727c9fe9
Amit Agrawal <lophandi0781@gmail.com>
2022-07-15 12:21:16 +0530

    Added login to COnfig

--- TestAuth/TestAuth/Home.aspx.cs
+++ TestAuth/TestAuth/Home.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Web;
 using System.Web.UI;
 
@@ -9,12 +10,14 @@ namespace TestAuth
         /// <summary>
         /// loginSuccessURL
         /// </summary>
-        private static string loginSuccessURL = "/LoginSuccess.aspx";
-        private static string loginSuccessURLFull = GetLoginSuccessUrl();
+        private static string loginSuccessURL = ConfigurationManager.AppSettings["LoginSuccessUrl"].ToString();       
         /// <summary>
         /// authURL
         /// </summary>
-        private static string authURL = string.Format("https://hdfc-accounts-api.skill-mine.com/authz-srv/authz?response_type=token&scope=openid%20profile%20email%20phone&redirect_uri={0}&flow_id=cf773255-5d29-4d0a-96e9-322026865a5c&client_id=8e28e7b4-cd75-4f54-87ed-c8c34ddf2971", loginSuccessURLFull);
+        //private static string authURL = string.Format("https://hdfc-accounts-api.skill-mine.com/authz-srv/authz?response_type=token&scope=openid%20profile%20email%20phone&redirect_uri={0}&flow_id=cf773255-5d29-4d0a-96e9-322026865a5c&client_id=8e28e7b4-cd75-4f54-87ed-c8c34ddf2971", loginSuccessURL);
+
+        private static string authURL = string.Format("https://hdfc-accounts-api.skill-mine.com/authz-srv/authz?response_type=token&scope=openid%20profile%20email%20phone&redirect_uri={0}&flow_id=80b9f3e9-ebd8-4925-ab23-ef3517577112&client_id=c748b0bb-3238-4338-ae76-0b9716f70202", loginSuccessURL);
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
