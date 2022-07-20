0e82de5a9a6a549bed072e40d347a9d05088661a
Amit Agrawal <lophandi0781@gmail.com>
2022-07-20 05:32:23 +0530

    Removed unwanted using

--- TestAuth/TestAuth/Welcome.aspx.cs
+++ TestAuth/TestAuth/Welcome.aspx.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using SkillMineAuth;
+﻿using SkillMineAuth;
+using System;
 using TestAuth.AuthRules;
 namespace TestAuth
 {
